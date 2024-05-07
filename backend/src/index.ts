636889303d5979454817007d2aaf97728511cefa
Andrei Petridean <petrideanandrei25@gmail.com>
2024-05-07 14:29:59 +0300

    chore: basic eslint and prettier configuration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,15 @@
-import express, { Express, Request, Response } from 'express'
-import dotenv from 'dotenv'
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 
-const app: Express = express()
-const port = process.env.PORT || 3500
+const app: Express = express();
+const port = process.env.PORT || 3500;
 
 app.get('/', (_req: Request, res: Response) => {
-  res.send('Express + TypeScript configuration')
-})
+  res.send('Express + TypeScript configuration');
+});
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`)
-})
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
